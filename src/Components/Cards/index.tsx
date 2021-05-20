1d4047a2f7a092e3e054bcc98bcc15073335112d
Flávio Renato <peudevbr@gmail.com>
2021-05-19 22:15:00 -0300

    Adicionando Modal de Peças

--- src/Components/Cards/index.tsx
+++ src/Components/Cards/index.tsx
@@ -1,44 +1,132 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "react-modal";
-import { A, P, Container, Card, Box, Content, H3, Image } from "./styles";
- 
-type CardsProps = {
-  products : {
-    name: string,
-    code: string,
-    brand: string,
-    subject: string,
-    model: string,
-    cost: number,
-    title: string
-  }
-}
+import {
+  A,
+  P,
+  Container,
+  Card,
+  Box,
+  Content,
+  H3,
+  Image,
+  ModalContainer,
+  ModalBackButton,
+  ModalAcceptButton,
+  ButtonsContainer,
+  DescriptionContainer
+} from "./styles";
 
-export default function Cards({ products, search}) {
+export default function Cards({ products, search }) {
   const [isCardModalOpen, setIsCardModalOpen] = useState(false);
+  const [cardModal, setCardModal] = useState({});
 
   function handleOpenCardModal() {
     setIsCardModalOpen(true);
+    Modal.setAppElement("body");
   }
 
   function handleCloseCardModal() {
     setIsCardModalOpen(false);
   }
+  function Display(element) {
+    console.log(element);
+  }
 
   let cardList = [];
 
-  for (var slice in products) {
-    if (
-      products[slice].name.toUpperCase().includes(search) ||
-      products[slice].model.toUpperCase().includes(search) ||
-      products[slice].code.toUpperCase().includes(search) ||
-      products[slice].brand.toUpperCase().includes(search) ||
-      products[slice].subject.toUpperCase().includes(search) ||
-      products[slice].title.toUpperCase().includes(search)
-    ) {
-      cardList.push(products[slice]);
+  if (search === "") {
+    cardList = [
+      {
+        name: "Placa Potência 220V",
+        code: "W10446925",
+        brand: "Brastemp/Consul",
+        subject: "Máquina de Lavar, Lavadora, Maquina de lavar",
+        model:
+          "BWB11ABBNA,BWL11ABBNA,BWL11AR,BWK11ABBNA,CWG11AB,CWK12ABBNA,CWL10B,CWL75A,CWK11AB,CWC10AB,BWC11AB,BWC09AB,BWC10BB,BWC07A,BWC08A,BWC10A,BWB09AB,BWL09BBBNA,BWB08A,BWB08AB",
+        cost: "180,00",
+        title: "potencia",
+      },
+      {
+        name: "Compressor 1/3+ R134A 220V",
+        code: "W10393823",
+        brand: "Embraco",
+        subject: "Freezer",
+        model: "FFI 12HBX",
+        cost: "480,00",
+        title: "Motor, Motor 1/3, , Motor 1/3+, Motor 1/3 +, motor freezer",
+      },
+      {
+        name: "Tubulação de cobre 1/4 ",
+        code: "100213250",
+        brand: "Eluma",
+        subject: "Split",
+        model: "Tubo flexível",
+        cost: "100,00 (kg)",
+        title:
+          "Cano 1/4, Cano de Cobre, Cano de Cobre 1/4, Tubo 1/4, Instalação, Instalaçao, Instalacao",
+      },
+      {
+        name: "Compressor Rotativo 9.000 Btus R22",
+        code: "326053935",
+        brand: "Brastemp/Consul",
+        subject: "split, condicionador de ar, ar condicionado",
+        model: "",
+        cost: "550,00",
+        title: "motor",
+      },
+      {
+        name: "Kit Placa BWL11 - W10356413 W10301604 326064442 (Similar)",
+        code: "7220060",
+        brand: "Alado",
+        subject: "multimarcas, similar, consul, brastemp",
+        model: "bwl11",
+        cost: "280,00",
+        title: "placa potencia, placa interface, placa potência",
+      },
+      {
+        name: "Kit Placa BWC10 Bivolt (Similar)",
+        code: "1447",
+        brand: "CP Placas",
+        subject: "multimarcas, similar, consul, brastemp",
+        model: "bwc10",
+        cost: "150,00",
+        title: "placa potencia, placa potência",
+      },
+      {
+        name: "Placa Eletrônica LT12F",
+        code: "70201326",
+        brand: "Electrolux",
+        subject: "Máquina de Lavar, Lavadora, Maquina de lavar",
+        model: "LT12f",
+        cost: "200,00",
+        title: "potencia",
+      },
+      {
+        name: "Trasmissão Completa",
+        code: "70201828",
+        brand: "Electrolux",
+        subject: "lavadora, máquina de lavar, maquina de lavar",
+        model: "",
+        cost: "450,00",
+        title: "mecanismo, mecanica, transmissao, mecânica",
+      },
+    ];
+  } else {
+    for (var slice in products) {
+      if (
+        products[slice].name.toUpperCase().includes(search) ||
+        products[slice].model.toUpperCase().includes(search) ||
+        products[slice].code.toUpperCase().includes(search) ||
+        products[slice].brand.toUpperCase().includes(search) ||
+        products[slice].subject.toUpperCase().includes(search) ||
+        products[slice].title.toUpperCase().includes(search)
+      ) {
+        cardList.push(products[slice]);
+      }
     }
   }
+
+  //href={`https://api.whatsapp.com/send?1=pt_BR&phone=558299802-2266&text=Tenho interesse na peça: ${card.code} - ${card.name}`}
 
   return (
     <Container>
@@ -53,15 +141,45 @@ export default function Cards({ products, search}) {
                 <H3>{card.name}</H3>
                 <P>{card.code}</P>
                 <P>R$ {card.cost}</P>
-                <A href={`https://api.whatsapp.com/send?1=pt_BR&phone=558299802-2266&text=Tenho interesse na peça: ${card.code} - ${card.name}`} className="button" target="_blank">Verificar</A>
-                
+                <A
+                  onClick={() => {
+                    setCardModal(card);
+                    handleOpenCardModal();
+                  }}
+                  className="button"
+                  target="_blank"
+                >
+                  Verificar
+                </A>
                 <Modal
                   isOpen={isCardModalOpen}
                   onRequestClose={handleCloseCardModal}
                 >
-                  <h2>{card.name}</h2>
+                  <ModalContainer>
+                    <div>
+                      <img src={`/images/parts/${cardModal.code}.png`} />
+                    </div>
+                    <div>
+                      <DescriptionContainer>
+                        <h1>{cardModal.name}</h1>
+                        <h3>{cardModal.brand}</h3>
+                        <h2>Código: {cardModal.code}</h2>
+                        <h2>R$ {cardModal.cost}</h2>
+                      </DescriptionContainer>
+                      <ButtonsContainer>
+                        <ModalAcceptButton
+                          href={`https://api.whatsapp.com/send?1=pt_BR& phone=558299802-2266&text=Tenho interesse na peça: ${cardModal. code} - ${cardModal.name} - ${cardModal.cost}`}
+                        >
+                          Solicitar Peça
+                          <img src={`/images/whatsapp.png`} alt="" />
+                        </ModalAcceptButton >
+                        <ModalBackButton onClick={handleCloseCardModal}>
+                          Voltar
+                        </ModalBackButton>
+                      </ButtonsContainer>
+                    </div>
+                  </ModalContainer>
                 </Modal>
-                
               </Content>
             </Box>
           </Card>
--- src/Components/Cards/styles.ts
+++ src/Components/Cards/styles.ts
@@ -1,29 +1,29 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
   width: 100vw;
   grid-template-columns: repeat(4, 300px);
-  justify-items:center;
+  justify-items: center;
   justify-content: center;
   margin-top: 120px;
-`
+`;
 export const Box = styled.div`
-  height: 360px; /* What??? */
+  height: 320px; /* What??? */
 
-  position: absolute; 
+  position: absolute;
   top: 20px;
   left: 20px;
   right: 20px;
   border: 20px;
   background: linear-gradient(45deg, #ebedee, #accbee);
-  box-shadow: 0 10px 20px rgba(0,0,0,0.1);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
   border-radius: 15px;
   display: flex;
   justify-content: center;
   align-items: center;
   transition: 0.7s;
-`
+`;
 export const H2 = styled.h2`
   position: absolute;
   top: 2px;
@@ -32,7 +32,7 @@ export const H2 = styled.h2`
   color: rgba(0, 0, 0, 0.02);
   transition: 0.7s;
   pointer-events: none;
-`
+`;
 
 export const A = styled.a`
   position: relative;
@@ -43,54 +43,54 @@ export const A = styled.a`
   color: #777;
   text-decoration: none;
   font-weight: 700;
-  box-shadow: 0 10px 20px rgba(0,0,0, 0.2);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
   margin-bottom: 0.3rem;
   margin-top: 0.3rem;
   transition: 0.9s;
   outline: none;
   cursor: pointer;
-`
+`;
 export const H3 = styled.h3`
   font-size: 1em;
   color: #777;
   min-height: 3.2rem;
   z-index: 1;
   transition: 0.7s;
-`
+`;
 
 export const P = styled.p`
   font-size: 1em;
   font-weight: 300;
   color: #777;
+  margin-top: 0.25rem;
   z-index: 1;
+
   transition: 0.7s;
-`
+`;
 
 export const Content = styled.div`
   padding: 20px;
   text-align: center;
-`
+`;
 export const Image = styled.img`
-  height: 12rem;
-  width: 12rem;
-  
-`
+  height: 8rem;
+  width: 8rem;
+`;
 
 export const Card = styled.div`
   position: relative;
-  width: 270px;
-  height: 400px;
-  box-shadow: inset 5px 5px 5px rgba(0,0,0,0.05),
-              inset -5px -5px 5px rgba(225,225,225,0.5),
-              5px 5px 5px rgba(0,0,0,0.05),
-              -5px -5px 5px rgba(225,225,225,0.5);
+  width: 260px;
+  height: 360px;
+  box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.05),
+    inset -5px -5px 5px rgba(225, 225, 225, 0.5),
+    5px 5px 5px rgba(0, 0, 0, 0.05), -5px -5px 5px rgba(225, 225, 225, 0.5);
   border-radius: 15px;
-  margin: 15px; 
+  margin: 15px;
 
   &:hover {
     ${Box} {
       transform: scale(1.25); //translateY(-50px);
-      box-shadow: 0 10px 40px rgba(0,0,0,0.2);
+      box-shadow: 0 10px 40px rgba(0, 0, 0, 0.2);
       //background: linear-gradient(45deg, #accbee, #D7FFFE)
     }
     ${H2} {
@@ -105,5 +105,76 @@ export const Card = styled.div`
     ${P} {
       //color: #FFF;
     }
-}
+  }
+`;
+
+export const ModalContainer = styled.div`
+  display: grid;
+  width: 80vw;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+  justify-content: center;
+  margin-top: 100px;
+
+  img {
+    height: 25rem;
+    width: 25rem;
+  }
+`;
+
+
+export const DescriptionContainer = styled.div`
+  display: block;
+  margin-top: 80px;
+
+  h2{
+    margin-bottom: 1rem;
+  }
+  h3{
+    margin-bottom: 1rem;
+  }
 `
+
+export const ModalBackButton = styled.button`
+  margin-left: 20px;
+  padding: 12px 20px;
+  background: #e5383b;
+  font-size: 1.5rem;
+  border-radius: 5px;
+  color: #ffffff;
+  font-weight: 700;
+  margin-bottom: 0.3rem;
+  margin-top: 0.3rem;
+  transition: 0.5s;
+  border: none;
+  cursor: pointer;
+`;
+
+export const ButtonsContainer = styled.div`
+display: flex;
+`
+
+export const ModalAcceptButton = styled.a`
+  
+  padding: 8px 16px;
+  background: #52b788;
+  font-size: 1.5rem;
+  border-radius: 5px;
+  color: #ffffff;
+  font-weight: 700;
+  margin-bottom: 0.3rem;
+  margin-top: 0.3rem;
+  transition: 0.5s;
+  border: none;
+  text-decoration: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  img {
+    margin-left: 0.5rem;
+    height: 50px;
+    width: 50px;
+
+  }
+`;
