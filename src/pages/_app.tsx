7151a8110aa9d1577b3d87860ce04ed54807b8ed
Flávio Renato <peudevbr@gmail.com>
2021-06-07 20:48:05 -0300

    Adicionando Title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from "next/app"
+import Head from 'next/head'
 import { GlobalStyle } from "../../styles/global"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Loja Servibras</title>
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle/>
     </>
